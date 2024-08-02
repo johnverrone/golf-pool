a4fbe6903c7b2e4aefa976c6fffec410dd9c9f3f
John Verrone <john.verrone@gmail.com>
2024-08-02 16:09:18 -0600

    prettier

--- src/lib/supabaseTypes.ts
+++ src/lib/supabaseTypes.ts
@@ -1,196 +1,182 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
 export type Database = {
-  public: {
-    Tables: {
-      entries: {
-        Row: {
-          created_at: string
-          id: number
-          name: string | null
-          paid: boolean
-          players: string[] | null
-          pool_id: number
-          team_name: string
-        }
-        Insert: {
-          created_at?: string
-          id?: number
-          name?: string | null
-          paid?: boolean
-          players?: string[] | null
-          pool_id: number
-          team_name: string
-        }
-        Update: {
-          created_at?: string
-          id?: number
-          name?: string | null
-          paid?: boolean
-          players?: string[] | null
-          pool_id?: number
-          team_name?: string
-        }
-        Relationships: [
-          {
-            foreignKeyName: "entries_pool_fkey"
-            columns: ["pool_id"]
-            isOneToOne: false
-            referencedRelation: "pools"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
-      pools: {
-        Row: {
-          created_at: string
-          id: number
-          name: string
-        }
-        Insert: {
-          created_at?: string
-          id?: number
-          name: string
-        }
-        Update: {
-          created_at?: string
-          id?: number
-          name?: string
-        }
-        Relationships: []
-      }
-      tiers: {
-        Row: {
-          created_at: string
-          players: string[] | null
-          pool_id: number
-          required_picks: number
-          tier_num: number
-        }
-        Insert: {
-          created_at?: string
-          players?: string[] | null
-          pool_id?: number
-          required_picks: number
-          tier_num: number
-        }
-        Update: {
-          created_at?: string
-          players?: string[] | null
-          pool_id?: number
-          required_picks?: number
-          tier_num?: number
-        }
-        Relationships: [
-          {
-            foreignKeyName: "tiers_pool_id_fkey"
-            columns: ["pool_id"]
-            isOneToOne: false
-            referencedRelation: "pools"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      [_ in never]: never
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+	public: {
+		Tables: {
+			entries: {
+				Row: {
+					created_at: string;
+					id: number;
+					name: string | null;
+					paid: boolean;
+					players: string[] | null;
+					pool_id: number;
+					team_name: string;
+				};
+				Insert: {
+					created_at?: string;
+					id?: number;
+					name?: string | null;
+					paid?: boolean;
+					players?: string[] | null;
+					pool_id: number;
+					team_name: string;
+				};
+				Update: {
+					created_at?: string;
+					id?: number;
+					name?: string | null;
+					paid?: boolean;
+					players?: string[] | null;
+					pool_id?: number;
+					team_name?: string;
+				};
+				Relationships: [
+					{
+						foreignKeyName: 'entries_pool_fkey';
+						columns: ['pool_id'];
+						isOneToOne: false;
+						referencedRelation: 'pools';
+						referencedColumns: ['id'];
+					}
+				];
+			};
+			pools: {
+				Row: {
+					created_at: string;
+					id: number;
+					name: string;
+				};
+				Insert: {
+					created_at?: string;
+					id?: number;
+					name: string;
+				};
+				Update: {
+					created_at?: string;
+					id?: number;
+					name?: string;
+				};
+				Relationships: [];
+			};
+			tiers: {
+				Row: {
+					created_at: string;
+					players: string[] | null;
+					pool_id: number;
+					required_picks: number;
+					tier_num: number;
+				};
+				Insert: {
+					created_at?: string;
+					players?: string[] | null;
+					pool_id?: number;
+					required_picks: number;
+					tier_num: number;
+				};
+				Update: {
+					created_at?: string;
+					players?: string[] | null;
+					pool_id?: number;
+					required_picks?: number;
+					tier_num?: number;
+				};
+				Relationships: [
+					{
+						foreignKeyName: 'tiers_pool_id_fkey';
+						columns: ['pool_id'];
+						isOneToOne: false;
+						referencedRelation: 'pools';
+						referencedColumns: ['id'];
+					}
+				];
+			};
+		};
+		Views: {
+			[_ in never]: never;
+		};
+		Functions: {
+			[_ in never]: never;
+		};
+		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
+			[_ in never]: never;
+		};
+	};
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, 'public'>];
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+	PublicTableNameOrOptions extends
+		| keyof (PublicSchema['Tables'] & PublicSchema['Views'])
+		| { schema: keyof Database },
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+		? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+				Database[PublicTableNameOrOptions['schema']]['Views'])
+		: never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
-    }
-    ? R
-    : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
-    : never
+	? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+			Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+			Row: infer R;
+		}
+		? R
+		: never
+	: PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] & PublicSchema['Views'])
+		? (PublicSchema['Tables'] & PublicSchema['Views'])[PublicTableNameOrOptions] extends {
+				Row: infer R;
+			}
+			? R
+			: never
+		: never;
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+	PublicTableNameOrOptions extends keyof PublicSchema['Tables'] | { schema: keyof Database },
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+		? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+		: never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
-    : never
+	? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+			Insert: infer I;
+		}
+		? I
+		: never
+	: PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+		? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+				Insert: infer I;
+			}
+			? I
+			: never
+		: never;
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
-  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+	PublicTableNameOrOptions extends keyof PublicSchema['Tables'] | { schema: keyof Database },
+	TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+		? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+		: never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
-    }
-    ? U
-    : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
-    : never
+	? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+			Update: infer U;
+		}
+		? U
+		: never
+	: PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+		? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+				Update: infer U;
+			}
+			? U
+			: never
+		: never;
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
-    | { schema: keyof Database },
-  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+	PublicEnumNameOrOptions extends keyof PublicSchema['Enums'] | { schema: keyof Database },
+	EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+		? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+		: never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+	? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+	: PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
+		? PublicSchema['Enums'][PublicEnumNameOrOptions]
+		: never;
